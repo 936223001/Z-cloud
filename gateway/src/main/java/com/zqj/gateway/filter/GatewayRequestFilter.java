69e271a001b3ef2f4edba1a3aaf7b1a9d6f85b86
zhuqijing <936223001@qq.com>
2021-04-25 16:20:45 +0800

    添加日志配置文件

--- gateway/src/main/java/com/zqj/gateway/filter/GatewayRequestFilter.java
+++ gateway/src/main/java/com/zqj/gateway/filter/GatewayRequestFilter.java
@@ -1,7 +1,9 @@
 package com.zqj.gateway.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -9,12 +11,19 @@ import org.springframework.util.Base64Utils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
+
 /***
  * @Description 网关过滤器 (防止客户端绕过网关，直接请求微服务。解决这个问题的大致思路是：在网关转发请求前，请求头部加入网关信息，然后在处理请求的微服务模块里定义全局拦截器，校验请求头部的网关信息，这样就能避免客户端直接访问微服务了)
  * @author zhuqijing
  * @date 2021/4/21
  * @Version 1.0
  **/
+@Slf4j
 @Component
 public class GatewayRequestFilter implements GlobalFilter {
     @Override
@@ -24,10 +33,29 @@ public class GatewayRequestFilter implements GlobalFilter {
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
 
+        //日志打印
+        printLog(exchange);
+
         // 在请求头添加网关密匙
         byte[] token = Base64Utils.encode(("zqj:gateway:123456").getBytes());
         ServerHttpRequest build = request.mutate().header("GatewayToken", new String(token)).build();
         ServerWebExchange newExchange = exchange.mutate().request(build).build();
         return chain.filter(newExchange);
+    }
+
+    private void printLog(ServerWebExchange exchange) {
+        URI url = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        LinkedHashSet<URI> uris = exchange.getAttribute(GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
+        URI originUri = null;
+        if (uris != null) {
+            originUri = uris.stream().findFirst().orElse(null);
+        }
+        if (url != null && route != null && originUri != null) {
+            log.info("转发请求：{}://{}{} --> 目标服务：{}，目标地址：{}://{}{}，转发时间：{}",
+                    originUri.getScheme(), originUri.getAuthority(), originUri.getPath(),
+                    route.getId(), url.getScheme(), url.getAuthority(), url.getPath(), LocalDateTime.now()
+            );
+        }
     }
 }
